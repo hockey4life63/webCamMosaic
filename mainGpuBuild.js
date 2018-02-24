da03f0f95270c8ac0f55e8231fe850a6ae3ca933
unknown <amelancon68@gmail.com>
2018-02-23 19:07:53 -0800

    removed so code for currently unused libary

--- mainGpuBuild.js
+++ mainGpuBuild.js
@@ -1,5 +1,5 @@
 // const Webcam = require('webcamjs')
-const gpu = new GPU()
+// const gpu = new GPU()
 const tileSize = 16
 const video = document.getElementById('cameraFeed')
 let streaming = false
@@ -20,34 +20,34 @@ let targetImageData = null
 let targetImage;
 let currentTileDiff = new Array((width / tileSize) * (height / tileSize)).fill(10000)
 
-const compareAllTiles = gpu.createKernel(function(targetImage, testTile) {
-    let xTileIndex = this.thread.x % this.constants.tiledWidth
-    let yTileIndex = Math.floor(this.thread.x / this.constants.tiledWidth)
-    let targetX = xTileIndex*this.constants.tileSize*4
-    let targetY = yTileIndex*this.constants.tileSize*4
-    let sum = 0
-    let startingIndex = targetY*this.constants.width*4+targetX
-    for (let col = 0; col < this.constants.tileSize*4; col+=4) {
-        for (let row = 0; row < this.constants.tileSize*4; row+=4) {
-            let r = targetImage[startingIndex+(row*this.constants.width+col*4)] - testTile[row*this.constants.tileSize+col*4]
-            let g = targetImage[startingIndex+(row*this.constants.width+col*4)+1] - testTile[row*this.constants.tileSize+col*4+1]
-            let b = targetImage[startingIndex+(row*this.constants.width+col*4)+2] - testTile[row*this.constants.tileSize+col*4+2]
-            sum+=abs(r)+abs(g)+abs(b)
-        }
-    }
-    sum = sum/(this.constants.tileSize*this.constants.tileSize)
-    return floor(sum+0.5)
-}, {
-    output:[(width / tileSize) * (height / tileSize) ],
-    constants:{tileSize, width, tiledWidth, tiledHeight},
-})
+// const compareAllTiles = gpu.createKernel(function(targetImage, testTile) {
+//     let xTileIndex = this.thread.x % this.constants.tiledWidth
+//     let yTileIndex = Math.floor(this.thread.x / this.constants.tiledWidth)
+//     let targetX = xTileIndex*this.constants.tileSize*4
+//     let targetY = yTileIndex*this.constants.tileSize*4
+//     let sum = 0
+//     let startingIndex = targetY*this.constants.width*4+targetX
+//     for (let col = 0; col < this.constants.tileSize*4; col+=4) {
+//         for (let row = 0; row < this.constants.tileSize*4; row+=4) {
+//             let r = targetImage[startingIndex+(row*this.constants.width+col*4)] - testTile[row*this.constants.tileSize+col*4]
+//             let g = targetImage[startingIndex+(row*this.constants.width+col*4)+1] - testTile[row*this.constants.tileSize+col*4+1]
+//             let b = targetImage[startingIndex+(row*this.constants.width+col*4)+2] - testTile[row*this.constants.tileSize+col*4+2]
+//             sum+=abs(r)+abs(g)+abs(b)
+//         }
+//     }
+//     sum = sum/(this.constants.tileSize*this.constants.tileSize)
+//     return floor(sum+0.5)
+// }, {
+//     output:[(width / tileSize) * (height / tileSize) ],
+//     constants:{tileSize, width, tiledWidth, tiledHeight},
+// })
 
 
-const compareDiff = gpu.createKernel(function(testTileDiff, currentTileDiff){
-    return currentTileDiff[this.thread.x] - testTileDiff[this.thread.x] 
-}, {
-    output:[(width / tileSize) * (height / tileSize) ],
-})
+// const compareDiff = gpu.createKernel(function(testTileDiff, currentTileDiff){
+//     return currentTileDiff[this.thread.x] - testTileDiff[this.thread.x] 
+// }, {
+//     output:[(width / tileSize) * (height / tileSize) ],
+// })
 
 const compareImageAndTIle = (targetImage, testTile)=>{
     let testArr = new Array(tiledHeight*tiledWidth).fill(0)
